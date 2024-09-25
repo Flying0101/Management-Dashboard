44d51245cd2cd625887241821070711d7c0dcd40
Flying0101 <stephano@live.se>
2024-09-25 13:56:18 +0200

    add: new screen breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "500px",
+      sm: "650px",
+      md: "810px",
+      lg: "1030px",
+      xl: "1520px",
+      xxl: "1750px",
+      xxxl: "2770px",
+    },
     extend: {
       colors: {
         background: "var(--background)",
